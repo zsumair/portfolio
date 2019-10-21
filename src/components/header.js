cc13b8e11925087985f4b78522d0b44fb08ce95d
syedzoheb1242@gmail.com <syedzoheb1242@gmail.com>
2019-10-21 22:59:19 +0530

    Added Animation

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,25 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 const Header = ({ siteTitle }) => (
   <header className="navbar">
     <h4 style={{ margin: 0 }}>
-      <Link
+      <AniLink
+        cover
+        direction="down"
         to="/"
+        duration={1}
+        bg="
+        url(https://source.unsplash.com/random)
+        center / cover   /* position / size */
+        no-repeat        /* repeat */
+        fixed            /* attachment */
+        padding-box      /* origin */
+        content-box      /* clip */
+        white            /* color */
+        "
         style={{
           color: `#F26157`,
           textDecoration: `none`,
@@ -15,14 +28,18 @@ const Header = ({ siteTitle }) => (
         }}
       >
         {siteTitle}
-      </Link>
+      </AniLink>
     </h4>
     <ul className="nav-links">
       <li>
-        <Link to="/about">About</Link>
+        <AniLink paintDrip to="/about" duration={1.5} hex="#F26157">
+          About
+        </AniLink>
       </li>
       <li>
-        <Link to="/work">Work</Link>
+        <AniLink fade to="/work" duration={0.3}>
+          Work
+        </AniLink>
       </li>
     </ul>
   </header>
